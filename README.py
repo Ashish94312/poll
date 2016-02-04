e0ff58e652a54ce8ee8bae42a200faef2ff86e83
Ashish94312 <astro94312@gmail.com>
2016-02-04 20:43:50 +0530

    Update README.py

--- README.py
+++ README.py
@@ -5,4 +5,8 @@ app=Flask(__name__)
 @app.route('/<name>')
 def index(name):
   return "hello {}".format(name)
+
+@app.route('/add/<num1>/<num2>')
+def add(num1,num2):
+  return '{} + {} = {}'.format(num1, num2, num1+num2)
 app.run(debug=True)
